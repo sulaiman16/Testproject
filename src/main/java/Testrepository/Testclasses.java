2000b3d1915b7438e241cc711b80acc357e4c99c
sulaiman16 <jallohsulaiman16@gmail.com>
2019-07-23 17:48:39 +0100

    first commit

---
+++ src/main/java/Testrepository/Testclasses.java
@@ -0,0 +1,11 @@
+package Testrepository;
+
+public class Testclasses {
+
+	public static void main(String[] args) {
+	
+		System.out.println("test git");
+
+	}
+
+}
---
+++ src/main/java/Testrepository/Testproject.java
@@ -0,0 +1,11 @@
+package Testrepository;
+
+public class Testproject {
+
+	public static void main(String[] args) {
+	
+		 System.out.println("Hello projectgithub");
+
+	}
+
+}
